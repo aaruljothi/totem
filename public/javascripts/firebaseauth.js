bf6eee83c1a7180fc7a300cf4f7c0f89048120af
aidanpetti <apetti1920@aol.com>
2018-04-28 16:32:46 -0400

    Firebase Auth

---
+++ public/javascripts/firebaseauth.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAOnOqleoQP9zJYmHFohDjIEIfZ1o8H1yI",
+    authDomain: "totem-umbc.firebaseapp.com",
+    databaseURL: "https://totem-umbc.firebaseio.com",
+    projectId: "totem-umbc",
+    storageBucket: "totem-umbc.appspot.com",
+    messagingSenderId: "811594686709"
+};
+firebase.initializeApp(config);
