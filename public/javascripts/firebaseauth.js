01200df7e3d47351f88e9b760c56b0cdcba797bc
aidanpetti <apetti1920@aol.com>
2018-04-28 23:41:17 -0400

    Firebase auth functions

--- public/javascripts/firebaseauth.js
+++ public/javascripts/firebaseauth.js
@@ -9,22 +9,65 @@ var config = {
 };
 firebase.initializeApp(config);
 
+function gottotribepage(){
+    window.location.href = "http://www.google.com";
+}
+
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-        console.log('new user', user.uid);
+        gottotribepage()
     }
 });
 
 function addnewuser(user, age, gender) {
     var userobject = {
-        'uuid': user.uid,
+        'username': user.uid,
         'email': user.email,
         'age': age,
-        'gender': gender
-    }
+        'gender': gender,
+        'intrest': '',
+        'zipcode': '07067'
+    };
 
-
+    $.ajax({
+        type: "POST",
+        url: "/register",
+        // The key needs to match your method's input parameter (case-sensitive).
+        data: JSON.stringify(userobject),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: gottotribepage(),
+        failure: function(errMsg) {
+            alert(errMsg);
+        }
+    });
 }
+
+function dummytest(user, age, gender) {
+    var userobject = {
+        'username': 'G7fRT3D22HQ',
+        'email': 'imtiredandwanttosleep@fuckthis.ugh',
+        'age': age,
+        'gender': gender,
+        'intrest': '',
+        'zipcode': '07067'
+    };
+
+    $.ajax({
+        type: "POST",
+        url: "/register",
+        // The key needs to match your method's input parameter (case-sensitive).
+        data: JSON.stringify(userobject),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: gottotribepage(),
+        failure: function(errMsg) {
+            alert(errMsg);
+        }
+    });
+}
+
+
 
 function readURL(input) {
     if (input.files && input.files[0]) {
