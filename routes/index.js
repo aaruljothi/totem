4a7e82346ece0f443faf645d178cbf372dff663d
JohnGrotke <john.grotke@gmail.com>
2018-04-29 00:29:50 -0400

    added error, logo, history

--- routes/index.js
+++ routes/index.js
@@ -10,12 +10,16 @@ router.get('/login', function(req, res, next) {
     res.render('login');
 });
 
-router.get('/preferences', function(req, res, next) {
-    res.render('preferences');
+router.get('/newUser', function(req, res, next) {
+    res.render('newUser');
 });
 
 router.get('/newPoll', function(req, res, next) {
     res.render('newPoll');
+});
+
+router.get('/history', function(req, res, next) {
+    res.render('history');
 });
 
 module.exports = router;
