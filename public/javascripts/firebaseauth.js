974fbdc5b30eb3211b2ad007d732678c959dfb02
aidanpetti <apetti1920@aol.com>
2018-04-28 23:12:27 -0400

    Firebase functions

--- public/javascripts/firebaseauth.js
+++ public/javascripts/firebaseauth.js
@@ -8,3 +8,35 @@ var config = {
     messagingSenderId: "811594686709"
 };
 firebase.initializeApp(config);
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        console.log('new user', user.uid);
+    }
+});
+
+function addnewuser(user, age, gender) {
+    var userobject = {
+        'uuid': user.uid,
+        'email': user.email,
+        'age': age,
+        'gender': gender
+    }
+
+
+}
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#blah')
+                .attr('src', e.target.result)
+                .width(150)
+                .height(200);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
